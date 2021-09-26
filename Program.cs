b1ed92a3abfe7fc680c2edad3980e6a7dd8117fa
Swapnil Nawale <swapnilnawale18@gmail.com>
2021-09-26 16:04:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CalculatorApi;
+namespace OperationUsingApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Operations");
+            Console.Write("Enter Var 1: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Var 2: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Addition: "+Operations.AddFn(a, b));
+            Console.WriteLine("Sub: " + Operations.SubFn(a, b));
+            Console.ReadKey();
+        }
+    }
+}
